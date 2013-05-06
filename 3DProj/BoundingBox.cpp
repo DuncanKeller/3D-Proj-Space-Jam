c4063a999a449a919ad4e88801037ae4b6662fbb
Duncan Keller <keller.duncan@gmail.com>
2013-05-06 15:01:36 -0400

    bounding box work

--- 3DProj/BoundingBox.cpp
+++ 3DProj/BoundingBox.cpp
@@ -16,3 +16,22 @@ BoundingBox::BoundingBox(float xMin, float xMax, float yMin, float yMax, float z
 BoundingBox::~BoundingBox(void)
 {
 }
+
+bool BoundingBox::Intersects(BoundingBox* box2)
+{
+	// check bounds
+	if(xMin > box2->xMax ||
+		box2->xMin > xMax) {
+		return false;
+	}
+	if(yMin > box2->yMax ||
+		box2->yMin > yMax) {
+		return false;
+	}
+	if(zMin > box2->zMax ||
+		box2->zMin > zMax) {
+		return false;
+	}
+
+	return true;
+}
--- 3DProj/BoundingBox.h
+++ 3DProj/BoundingBox.h
@@ -5,6 +5,8 @@ public:
 	BoundingBox(float, float, float, float, float, float);
 	~BoundingBox(void);
 
+	bool Intersects(BoundingBox* box2);
+
 private:
 
 	float xMax, xMin, yMax, yMin, zMax, zMin;
